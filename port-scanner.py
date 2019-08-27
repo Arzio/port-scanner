dab49b1e651d73d00e7eb35827ef641a0ada3587
Felipe Matheus <fmalbernaz@protonmail.com>
2019-08-27 13:59:55 -0300

    Une o algoritmo de scan de portas TCP com o de UDP

--- port-scanner.py
+++ port-scanner.py
@@ -1,13 +1,45 @@
 import sys
-import socket
 import re
 import datetime
+import socket
+from enum import Enum
+
+class ConnectionMethod(Enum):
+    TCP = 1,
+    UDP = 2
 
 def help():
-    print("Yee")
+    raise NotImplementedError
+
+def port_scan(ip: str, min: int, max: int, method: ConnectionMethod):
+    if method == ConnectionMethod.TCP:
+        name = "TCP"
+    elif method == ConnectionMethod.UDP:
+        name = "UDP"
+
+    print("Scanning {} ports at {}".format(name, ip))
+    start = datetime.datetime.now()
+    
+    for port in range(min, max + 1):
+        if method == ConnectionMethod.TCP:
+            con = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        
+        elif method == ConnectionMethod.UDP:
+            con = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+        dest = (ip, port)
+
+        if con.connect_ex(dest) == 0:
+            print("{} port {} is open!".format(name, port))
+
+        con.close()
+
+    time_taken = (datetime.datetime.now() - start).total_seconds()
+    print("{} scanning finished, it took {} seconds".format(name, time_taken))
 
 def main():
     min = max = ip = None
+    udp = tcp = False
 
     for arg_index in range(len(sys.argv)):
         arg = sys.argv[arg_index]
@@ -21,30 +53,25 @@ def main():
 
         elif arg in ("-t", "--to"):
             max = int(sys.argv[arg_index + 1])
+
+        elif arg == "--udp":
+            udp = True
+        
+        elif arg == "--tcp":
+            tcp = True
         
         elif re.match(r"^(\d{1,3}\.){3}\d{1,3}$", arg):
             ip = arg
 
-    if None in (min, max, ip):
+    if None in (min, max, ip) and (tcp == False and udp == False):
         print("Invalid args")
-        exit
+        exit()
 
-    start = datetime.datetime.now()
+    if tcp == True:
+        port_scan(ip, min, max, ConnectionMethod.TCP)
 
-    print("Scanning TCP ports at {}".format(ip))
-    
-    for port in range(min, max + 1):
-        con = socket.socket()
-        dest = (ip, port)
-
-        if con.connect_ex(dest) == 0:
-            print("TCP Port {} is open!".format(port))
-
-        con.close()
-
-    time_taken = (datetime.datetime.now() - start).total_seconds()
-
-    print("Scanning finished, it took {} seconds".format(time_taken))
+    if udp == True:
+        port_scan(ip, min, max, ConnectionMethod.UDP)
 
 if __name__ == "__main__":
     main()
